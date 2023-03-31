2517f8faba9ae54dc23a3b38da56ca33f141e5b0
Jean M Favre <jfavre@cscs.ch>
2023-03-31 10:39:51 +0200

    Ascent cleanup

--- miniapp/cuda/AscentAdaptor.h
+++ miniapp/cuda/AscentAdaptor.h
@@ -1,13 +1,6 @@
 #ifndef AscentAdaptor_h
 #define AscentAdaptor_h
 
-#include <ascent/ascent.hpp>
-#include "conduit_blueprint.hpp"
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <string>
-
 namespace AscentAdaptor
 {
   void Initialize();
